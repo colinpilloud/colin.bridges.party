ed3c9cc5fbede962672b9f89dd2b8a366372c45d
Colin Bridges <colin@bridges.party>
2026-01-05 11:55:46 -0800

    png favicon, improved copy, meta tags

--- src/components/portfolio/FutureFlipCity.tsx
+++ src/components/portfolio/FutureFlipCity.tsx
@@ -31,9 +31,8 @@ export function FutureFlipCity() {
         >
           this site
         </Link>{" "}
-        was written in collaboration with a friend of mine who's an iOS
-        engineer, in order to support{" "}
-        <span className="font-semibold text-secondary">
+        was written in tandem with an iOS engineer friend, in order to support{" "}
+        <span className="text-secondary font-semibold">
           Flip City Future League
         </span>{" "}
         events.
@@ -48,8 +47,8 @@ export function FutureFlipCity() {
       <Prose>
         The tournament organizer uses an iOS app to manage the event, and
         updates are pushed into{" "}
-        <span className="font-semibold text-secondary">Firebase</span>. This{" "}
-        <span className="font-semibold text-secondary">
+        <span className="text-secondary font-semibold">Firebase</span>. This{" "}
+        <span className="text-secondary font-semibold">
           TypeScript / React webapp
         </span>{" "}
         reads those updates and renders the tournament's pairings, scores, and
